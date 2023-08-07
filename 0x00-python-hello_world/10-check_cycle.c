9cc08340a9a4253714759ce10afbb4445292f3ba
badreaz <baz.23.2019@gmail.com>
2023-08-07 21:58:10 +0400

    edit 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- *  * new_list - recreate a list of listint_t values.
- *   * @values: pointer to pointer to listint_t.
- *    * @new: new value to add.
- *     * @len: number of nodes.
- *      *
- *       * Return: pointer to pointer to listint_t.
- *        */
-const listint_t **new_list(const listint_t **values,
-		const listint_t *new, int len)
-{
-	const listint_t **list;
-	int i;
-
-	list = malloc(len * sizeof(new));
-	if (list == NULL)
-		exit(98);
-	for (i = 0; i < len - 1; i++)
-		list[i] = values[i];							list[i] = new;
-	free(values);
-	return (list);
-}
-
-/**
  * check_cycle - check if a singly linked listhas a cycle.
  * @list: linked list.
  *
@@ -31,24 +8,18 @@ const listint_t **new_list(const listint_t **values,
  */
 int check_cycle(listint_t *list)
 {
-	const listint_t **values;
-	size_t i, num = 0;
+	const listint_t *turtle, *rabbit;
 
-	values = NULL;
-	while (head)
+	if (!list)
+		return (0);
+
+	turtle = rabbit = list;
+	while (list)
 	{
-		for (i = 0; i < num; i++)
-		{
-			if (head == values[i])
-			{
-				free(values);
-				return (1);
-			}
-		}
-		num++;
-		values = new_list(values, head, num);
-		head = head->next;
+		turtle = turtle->next;
+		rabbit = rabbit->next->next;
+		if (turtle == rabbit)
+			return (1);
 	}
-	free(values);
 	return (0);
 }
