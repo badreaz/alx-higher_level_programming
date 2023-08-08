ed488de4b628688cd3d5bede3aa7402d12eaae5c
badreaz <baz.23.2019@gmail.com>
2023-08-08 18:59:37 +0400

    print one character at a time

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,5 @@ def uppercase(str):
     for c in str:
         if ord(c) in range(ord('a'), ord('z') + 1):
             chr(ord(c) - 20)
-    print(str)
+        print("{}".format(c), end="")
+    print()
