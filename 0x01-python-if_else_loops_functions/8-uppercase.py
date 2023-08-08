21b30e0772542fbc000f65df8adbe5e39393aa21
badreaz <baz.23.2019@gmail.com>
2023-08-08 19:01:39 +0400

    store new value

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for c in str:
         if ord(c) in range(ord('a'), ord('z') + 1):
-            chr(ord(c) - 20)
+            c = chr(ord(c) - 20)
         print("{}".format(c), end="")
     print()
