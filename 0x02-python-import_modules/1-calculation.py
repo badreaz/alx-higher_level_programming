8d92315ab2193f69ad4eceac9a1cba404ce79fcb
badreaz <baz.23.2019@gmail.com>
2023-08-12 00:40:51 +0400

    fix code sstyle

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
-    b  = 5
+    b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, round(div(a, b))))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
