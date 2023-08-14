596e9f94d60e6c404d95f6a6617b0a373281e7a1
badreaz <baz.23.2019@gmail.com>
2023-08-14 17:11:37 +0400

    create 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        maximum = my_list[0]
+        for i in range(1, len(my_list)):
+            if maximum < my_list[i]:
+                maximum = my_list[i]
+        return maximum
+    else:
+        return None
