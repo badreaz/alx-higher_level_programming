5792ad6a7181730cda0988585c23e2a9d9ac9744
badreaz <baz.23.2019@gmail.com>
2023-08-15 00:13:01 +0400

    change reverse method

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,26 +1,6 @@
 #include "lists.h"
 
 /**
- *  * add_nodeint - adds a new node at the beginning of a listint_t list.
- *   * @head: pointer to listint_t.
- *    * @n: number to add.
- *     *
- *      * Return: pointer to new element.
- *       */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-		listint_t *new;
-
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		new->next = *head;
-		*head = new;
-		return (new);
-}
-
-/**
  * is_palindrome -checks if a singly linked list is a palindrome.
  * @head: linked list.
  *
@@ -28,35 +8,26 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 int is_palindrome(listint_t **head)
 {
-	int i, j;
-	listint_t *ptr = *head, *reverse, *free;
+	int i;
+	listint_t *ptr = *head, *reverse = *head;
+	listint_t *prev, *next;
 
+	prev = next = NULL;
 	if (!head || !*head)
 		return (1);
-	add_nodeint(&reverse, ptr->n);
-	reverse->next = NULL;
-	for (i = 1; ptr->next; i++)
+	while (reverse)
 	{
-		add_nodeint(&reverse, ptr->next->n);
-		ptr = ptr->next;
+		next = reverse->next;
+		reverse->next = prev;
+		prev = reverse;
+		reverse = next;
 	}
-	if (i % 2)
-	{
-		free_listint(reverse);
-		return (0);
-	}
-	ptr = *head;
-	free = reverse;
-	for (j = 0; j < i / 2; j++)
+	for (i = 0; ptr->next; i++)
 	{
 		if (ptr->n != reverse->n)
-		{
-			free_listint(free);
 			return (0);
-		}
 		ptr = ptr->next;
 		reverse = reverse->next;
 	}
-	free_listint(free);
 	return (1);
 }
