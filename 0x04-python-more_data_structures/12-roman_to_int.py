4e9c44de1cbd6521a2f7a9d96e3f8a07c4b5864f
badreaz <baz.23.2019@gmail.com>
2023-08-16 21:31:13 +0400

    handle empty string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     num = 0
     values = []
     roman_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
+    if not roman_string or type(roman_string) != str:
+        return 0
     for c in roman_string:
         values.append(roman_int[c])
     for i in range(len(values) - 1):
