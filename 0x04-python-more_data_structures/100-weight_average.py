dcb9d2175ae2eb2029390a35e6da56c875761d0d
badreaz <baz.23.2019@gmail.com>
2023-08-16 21:44:14 +0400

    omplete calculation

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,9 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     result = 0
+    denom = 0
     for i in my_list:
         result += i[0] * i[1]
+        denom += i[1]
+    result /= denom
     return result
