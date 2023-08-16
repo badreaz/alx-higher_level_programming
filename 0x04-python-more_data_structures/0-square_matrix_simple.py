5c1fee5c562a937ed973bf7cf6e94c97f2647f94
badreaz <baz.23.2019@gmail.com>
2023-08-16 17:14:52 +0400

    fix syntax error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new = []
     for line in range(len(matrix)):
-        for raw in range(len(matrix[line)):
+        for raw in range(len(matrix[line])):
             new[line][raw] = matrix[line][raw] ** 2
     return new
