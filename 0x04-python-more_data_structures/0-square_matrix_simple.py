45042598712b3029750afee48759669283c14e4a
badreaz <baz.23.2019@gmail.com>
2023-08-16 17:10:58 +0400

    create 0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = []
+    for line in range(len(matrix)):
+        for raw in range(len(matrix[line)):
+            new[line][raw] = matrix[line][raw] ** 2
+    return new
