535f4c5f0b840d145b3d6053252e9d06768014b9
badreaz <baz.23.2019@gmail.com>
2023-08-16 21:29:18 +0400

    store value in a list

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     num = 0
-    roman_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    for i in range(len(roman_string) + 1):
-        current = roman_int[roman_string[i]]
-        nexto = roman_int[roman_string[i + 1]]
-        if current >= nexto:
-            num += current
+    values = []
+    roman_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
+    for c in roman_string:
+        values.append(roman_int[c])
+    for i in range(len(values) - 1):
+        if values[i] >= values[i + 1]:
+            num += values[i]
         else:
-            num -= current
+            num -= values[i]
+    num += values[-1]
+    return num
