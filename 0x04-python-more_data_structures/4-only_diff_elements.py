9565fe0d009078b317ea4be19e2108110ebe778f
badreaz <baz.23.2019@gmail.com>
2023-08-16 19:44:39 +0400

    use XOR operator

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def only_deff_elements(set_1, set_2):
-    deff = []
-    for element in set_1:
-        if element not in set_2:
-            deff.append(element)
-    return deff
+    return set_1 ^ set_2
