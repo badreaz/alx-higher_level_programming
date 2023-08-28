558e4d52bb5c67d326689d84804301a509e66936
badreaz <baz.23.2019@gmail.com>
2023-08-28 18:47:38 +0400

    edit exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end='')
-        except:
+        except Exception as err:
             break
     print()
     return i + 1
