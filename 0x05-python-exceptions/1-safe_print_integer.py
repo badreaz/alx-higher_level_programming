67e320d007c505a0e48767fca0c31607c1337005
badreaz <baz.23.2019@gmail.com>
2023-08-28 18:46:48 +0400

    edit exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
-        print(f"{value} is not an integer")
+    except Exception as err:
         return False
