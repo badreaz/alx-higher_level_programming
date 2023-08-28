2341ca3d966a0597a6279c390a433306092ef656
badreaz <baz.23.2019@gmail.com>
2023-08-28 20:58:41 +0400

    add ValueError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    num = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except (IndexError, ValueError):
+            num += 1
+        except (TypeError, IndexError, ValueError):
             pass
     print()
-    return i
+    return num
