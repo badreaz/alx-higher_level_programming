1a7dbc4c81e4e7a7b0992708827d636a1374fa9a
badreaz <baz.23.2019@gmail.com>
2023-08-28 21:03:31 +0400

    edit print format

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         ans = None
     finally:
-        print("Inside result: {:d}".format(ans))
+        print("Inside result: {}".format(ans))
         return ans
