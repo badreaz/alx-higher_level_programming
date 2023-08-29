507903fbbbcfdd7c964573999d7e7fe4849fa71a
badreaz <baz.23.2019@gmail.com>
2023-08-29 17:23:50 +0400

    create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Square class
+
+    Args:
+        size (int):  size of the square.
+
+    Attributes:
+        __size (int): size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
