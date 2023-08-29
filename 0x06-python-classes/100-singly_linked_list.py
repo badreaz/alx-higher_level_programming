6a55f4c58119518134b623bd8f82727c60afb573
badreaz <baz.23.2019@gmail.com>
2023-08-29 22:21:02 +0400

    create 100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" Node & SinglyLinkedList class module """
+
+
+class Node:
+    """ defines a node of a singly linked list """
+    def __init__(self, data, next_node=None):
+        """ 
+        Args:
+            data (int): node data
+            next_node (:obj:'Node', optional): pointer to next node.
+        """
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """ retrieve __data value """
+        return self.__data
+
+    @propert
+    def next_node(self):
+        """ retieve __next_node value """
+        return self.__next_node
+
+    @data.setter
+    def data(self, value):
+        """ set __data """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @nex_node.setter
+    def next_node(self, value):
+        """ set __next_node """
+        if not isinstance(value, Node) || value != None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+class SinglyLinkedList:
+    """ defines a singly linked list """
+
+    def __init__(self):
+        """ simple instantation """
+        self.__head = None
+
+    def __str__(self):
+        """ printable """
+        line = ""
+        node = self.__head
+        while node:
+            line += str(node.data) + '\n'
+            node = node.next
+        line -= '\n'
+        return line
+
+    def sorted_insert(self, value):
+        """ insert new node in a sorted position """
+        if self.__head == None:
+            self.__head = Node(value)
+            return
+        if not self.__head.next:
+            self.__head.next_node(Node(value))
+            return
+        node = self.__head
+        while node.next:
+            if value >= node.next.data:
+                next_node = Node(value, node.next)
+                node.next_node(next_node)
+                return
+            node = node.next
+        node.next_node(Node(value))
