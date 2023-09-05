404bb1252e0f1bb0af3131204ea43462246ba756
badreaz <baz.23.2019@gmail.com>
2023-09-05 21:56:35 +0400

    add task 4 files

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" text_indentation definition module """
+
+
+def text_indentation(text):
+    """ prints a text with 2 new lines after each of these characters '.', '?' and ':' """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    result = ""
+    for c in text:
+        if c in ['.', '?', ':']:
+            result += c + "\n\n"
+            space = False
+        elif c == ' ' and space:
+            result += c
+        elif c != ' ':
+            result += c
+            space = True
+    print(result.strip(), end='')
