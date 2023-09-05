fd050b1e96b1733311e9fcffd7adcafea62670c3
badreaz <baz.23.2019@gmail.com>
2023-09-05 19:58:23 +0400

    create 2-matrix_divided.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """ add 2 integers """
-    if type(a) is not int or float:
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) is not int or float:
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return int(a + b)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module defines 'matrix_divided' """
+
+
+def matrix_divided(matrix, div):
+    """ divides all elements of a matrix """
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division bby zero")
+    msize = len(matrix[0])
+    result = []
+    for row in matrix:
+        if len(row) != msize:
+            raise TypeError("Each row of the matrix must have the same size")
+        new = []
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new.append(round(element / div))
+        result.append(new)
+    return result
