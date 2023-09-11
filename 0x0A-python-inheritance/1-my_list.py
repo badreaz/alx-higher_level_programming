5a4df080f9ea0f9fc2099f1826fdc3af7c79196d
badreaz <baz.23.2019@gmail.com>
2023-09-11 17:46:37 +0400

    create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" MyList class module """
+
+class MyList(list):
+    """ class that inherits from list """
+    def print_sorted(self):
+        """ prints the list in ascending order """
+        print(sorted(self))
