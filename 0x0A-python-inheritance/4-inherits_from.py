a7d7cc326479136477b096a39e0944c604d42f81
badreaz <baz.23.2019@gmail.com>
2023-09-11 18:24:06 +0400

    create 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" inherits_from definition module """
+
+
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of
+    a class that inherited from the specified class;
+    othrwise False """
+    if issubclass(obj, a_class):
+        return True
+    return False
