e799d86b21bc3022bdffb721c2cc2396f85decd1
badreaz <baz.23.2019@gmail.com>
2023-09-12 21:36:15 +0400

    create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Student class module """
+
+
+class Student():
+    """ class that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ instantation
+        Args:
+            first_name: student first name.
+            last_name: student last name
+            age: student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation
+        Args:
+            attrs: sttributes to represent.
+        """
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            dic = {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+            return dic
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replacess all attributes of the instance """
+        for k, v in json.items():
+            setattr(self, k, v)
