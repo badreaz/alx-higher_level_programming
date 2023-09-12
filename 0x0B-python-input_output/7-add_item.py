41a03bbb762c2a8bc1d2c3d200dcf705e4f7e8e2
badreaz <baz.23.2019@gmail.com>
2023-09-12 19:47:45 +0400

    create 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" adds all arguments to a python list, then save them to a file """
+import json, sys, os.path
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+my_obj = []
+if os.path.isfile(filename):
+    my_obj = load_from_json_file(filename)
+for i in range(1, len(sys.argv)):
+    my_obj.append(sys.argv[i])
+save_to_json_file(my_obj, filename)
