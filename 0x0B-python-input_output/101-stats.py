a2129bf42ecca2bf833a1ff627e5f01e534b4b96
badreaz <baz.23.2019@gmail.com>
2023-09-12 23:07:19 +0400

    create 101-stats.py

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" reads stdin line by line and computes metrics """
+import sys
+
+
+
+size = 0
+lnum = 1
+vcodes = ['200', '301', '400', '401', '403', '404', '405', '500']
+codes = {}
+
+try:
+    for line in sys.stdin:
+        if lnum == 10:
+            print("File size: {}".format(size))
+            for k in sorted(codes):
+                print("{}: {}".format(k, codes[k]))
+            lnum = 1
+        else:
+            lnum += 1
+
+        line = line.split()
+        size += int(line[-1])
+        if line[-2] in vcodes:
+            if not codes.get(line[-2]):
+                codes[line[-2]] = 1
+            else:
+                codes[line[-2]] += 1
+    print("File size: {}".format(size))
+    for k in sorted(codes):
+        print("{}: {}".format(k, codes[k]))
+
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+    for k in sorted(codes):
+        print("{}: {}".format(k, codes[k]))
+    raise
