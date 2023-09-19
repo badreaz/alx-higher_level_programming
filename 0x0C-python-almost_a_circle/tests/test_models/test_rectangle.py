cf999673e9f38247a65a9754a300b4316093250c
badreaz <baz.23.2019@gmail.com>
2023-09-19 16:20:41 +0400

    add new tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ rectangle class tests module """
 import unittest
+import os
+import io
 from models.rectangle import Rectangle
 
 
@@ -12,27 +14,54 @@ class TestRectangle(unittest.TestCase):
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
         r3 = Rectangle(10, 2, 0, 4, 12)
-        self.assertEqual(r1.id, 3)
-        self.assertEqual(r2.id, 4)
+        self.assertEqual(r1.id, 9)
+        self.assertEqual(r2.id, 10)
         self.assertEqual(r3.id, 12)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r2.width, 2)
         self.assertEqual(r3.height, 2)
         self.assertEqual(r2.x, 0)
         self.assertEqual(r3.y, 4)
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+            Rectangle(1, "2")
+            Rectangle(1, 2, "3")
+
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+            Rectangle(1, -2)
+            Rectangle(0, 2)
+            Rectangle(1, 0)
+            Rectangle(1, 2, -3)
+            Rectangle(1, 2, 3, -4)
 
     def test_str(self):
         """ tests for the str method """
         r1 = Rectangle(4, 6, 2, 1, 12)
-        r2 = Rectangle(5, 5, 1)
+        r2 = Rectangle(5, 5, 1, 0, 10)
         r1_str = "[Rectangle] (12) 2/1 - 4/6"
-        r2_str = "[Rectangle] (5) 1/0 - 5/5"
+        r2_str = "[Rectangle] (10) 1/0 - 5/5"
         self.assertEqual(str(r1), r1_str)
         self.assertEqual(str(r2), r2_str)
 
+    def test_display(self):
+        """ tests display mmethod """
+        r1 = Rectangle(2, 3, 0, 0 ,5)
+        r2 = Rectangle(2, 3, 1, 0 ,5)
+        r3 = Rectangle(2, 3, 1, 1 ,5)
+        r1_d = "##\n##\n##\n"
+        r2_d = " ##\n ##\n ##\n"
+        r3_d = "\n ##\n ##\n ##\n"
+        r1.display()
+        self.assertEqual(r1_d, io.StringIO().getvalue())
+        r2.display()
+        self.assertEqual(r2_d, io.StringIO().getvalue())
+        r3.display()
+        self.assertEqual(r3_d, io.StringIO().getvalue())
+
     def test_update(self):
         """ tests for update method """
-        r = Rectangle(10, 10, 10, 10)
+        r = Rectangle(10, 10, 10, 10, 8)
         r1_str = "[Rectangle] (8) 10/10 - 10/10"
         self.assertEqual(str(r), r1_str)
         r.update(height=1)
@@ -54,10 +83,42 @@ class TestRectangle(unittest.TestCase):
         r7_str = "[Rectangle] (6) 4/5 - 2/3"
         self.assertEqual(str(r), r7_str)
 
+    def test_create(self):
+        """ tests for create base class method """
+        r1 = Rectangle.create(**{ 'id': 89 })
+        self.assertEqual(str(r1), str(Rectangle(1, 1, 0, 0, 89)))
+        r2 = Rectangle.create(**{ 'id': 89, 'width': 1 })
+        self.assertEqual(str(r2), str(Rectangle(1, 1, 0, 0, 89)))
+        r3 = Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2 })
+        self.assertEqual(str(r3), str(Rectangle(1, 2, 0, 0, 89)))
+        r4 = Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 })
+        self.assertEqual(str(r4), str(Rectangle(1, 2, 3, 0, 89)))
+        r5 = Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 })
+        self.assertEqual(str(r5), str(Rectangle(1, 2, 3, 4, 89)))
+
+    def test_from_to_file(self):
+        """ tests for save_to_file, load_from_file methods """
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual("[]", f.read())
+        os.remove("Rectangle.json")
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual("[]", f.read())
+        os.remove("Rectangle.json")
+        Rectangle.save_to_file([Rectangle(1, 2, 0, 0, 6)])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(len(f.read()), 52)
+        load = Rectangle.load_from_file()
+        self.assertTrue(type(load[0]) == Rectangle)
+        os.remove("Rectangle.json")
+        load = Rectangle.load_from_file()
+        self.assertEqual(load, [])
+
     def test_to_dictionary(self):
         """ tests for dictionary method """
-        r1 = Rectangle(10, 2, 1, 9)
-        r1_dictionary = {'x': 1, 'y': 9, 'id': 6, 'height': 2, 'width': 10}
+        r1 = Rectangle(10, 2, 1, 9, 11)
+        r1_dictionary = {'x': 1, 'y': 9, 'id': 11, 'height': 2, 'width': 10}
         self.assertEqual(r1.to_dictionary(), r1_dictionary)
         self.assertEqual(type(r1.to_dictionary()), dict)
         r2 = Rectangle(1, 1)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ square class tests module """
 import unittest
+import os
+import io
 from models.square import Square
 
 
@@ -13,20 +15,31 @@ class TestSquare(unittest.TestCase):
         s1 = Square(5)
         s2 = Square(2, 2)
         s3 = Square(3, 1, 3)
-        self.assertEqual(str(s1), "[Square] (9) 0/0 - 5")
+        s4 = Square(1, 2, 3, 4)
+        self.assertEqual(str(s1), "[Square] (17) 0/0 - 5")
         self.assertEqual(s1.area(), 25)
-        self.assertEqual(str(s2), "[Square] (10) 2/0 - 2")
+        self.assertEqual(str(s2), "[Square] (18) 2/0 - 2")
         self.assertEqual(s2.area(), 4)
-        self.assertEqual(str(s3), "[Square] (11) 1/3 - 3")
+        self.assertEqual(str(s3), "[Square] (19) 1/3 - 3")
+        self.assertEqual(str(s4), "[Square] (4) 2/3 - 1")
         self.assertEqual(s3.area(), 9)
         self.assertEqual(s1.size, 5)
         s1.size = 10
         self.assertEqual(s1.size, 10)
+        with self.assertRaises(TypeError):
+            Square("1")
+            Square(1, "2")
+
+        with self.assertRaises(ValueError):
+            Square(-1)
+            Square(1, -2)
+            Square(1, 2, -3)
+            Square(0)
 
     def test_update(self):
         """ tests for update method """
 
-        s1 = Square(5)
+        s1 = Square(5, 0, 0, 14)
         self.assertEqual(str(s1), "[Square] (14) 0/0 - 5")
         s1.update(10)
         self.assertEqual(str(s1), "[Square] (10) 0/0 - 5")
@@ -43,16 +56,48 @@ class TestSquare(unittest.TestCase):
         s1.update(size=7, id=89, y=1)
         self.assertEqual(str(s1), "[Square] (89) 12/1 - 7")
 
+    def test_create(self):
+        """ test cases for create base class method """
+
+        s1 = Square.create(**{ 'id': 89 })
+        self.assertEqual(str(s1), str(Square(1, 0, 0, 89)))
+        s2 = Square.create(**{ 'id': 89, 'size': 1 })
+        self.assertEqual(str(s2), str(Square(1, 0, 0, 89)))
+        s3 = Square.create(**{ 'id': 89, 'size': 1, 'x': 2 })
+        self.assertEqual(str(s3), str(Square(1, 2, 0, 89)))
+        s4 = Square.create(**{ 'id': 89, 'size': 1, 'x': 2, 'y': 3 })
+        self.assertEqual(str(s4), str(Square(1, 2, 3, 89)))
+
+    def test_from_to_file(self):
+        """ tests for save_to_file, load_from_file methods """
+
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
+        os.remove("Square.json")
+        Square.save_to_file([])
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
+        os.remove("Square.json")
+        Square.save_to_file([Square(1, 0, 0, 6)])
+        with open("Square.json", "r") as f:
+            self.assertEqual(len(f.read()), 38)
+        load = Square.load_from_file()
+        self.assertTrue(type(load[0]) == Square)
+        os.remove("Square.json")
+        load = Square.load_from_file()
+        self.assertEqual(load, [])
+
     def test_to_dictionary(self):
         """ tests for to_dictionary method """
 
-        s1 = Square(10, 2, 1)
+        s1 = Square(10, 2, 1, 12)
         self.assertEqual(str(s1), "[Square] (12) 2/1 - 10")
         s1_dictionary = s1.to_dictionary()
         self.assertEqual(s1_dictionary, {'id': 12, 'x': 2, 'size': 10, 'y': 1})
         self.assertEqual(type(s1_dictionary), dict)
         s2 = Square(1, 1)
-        self.assertEqual(str(s2), "[Square] (13) 1/0 - 1")
+        self.assertEqual(str(s2), "[Square] (20) 1/0 - 1")
         s2.update(**s1_dictionary)
         self.assertEqual(str(s2), "[Square] (12) 2/1 - 10")
         self.assertFalse(s1 == s2)
