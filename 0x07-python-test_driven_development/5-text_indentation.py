18320644c0487f1e0eb84ef40a9ea0db66b11fd9
badreaz <baz.23.2019@gmail.com>
2023-10-01 21:17:10 +0400

    fix code style

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,9 @@
 
 
 def text_indentation(text):
-    """ prints a text with 2 new lines after each of these characters '.', '?' and ':' """
+    """ prints a text with 2 new lines after each
+    of these characters '.', '?' and ':'
+    """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
