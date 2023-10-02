c245066041852130173af69965cc35c21a7f7068
badreaz <baz.23.2019@gmail.com>
2023-10-02 18:30:48 +0400

    create 102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,20 @@
+#include "Python.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_python_string - prints python string information
+ * @p: string object.
+ */
+void print_python_string(PyObject *p)
+{
+	printf("[.] string object info\n");
+	if (PyUnicode_check(p))
+	{
+		printf("  type: %s\n", PyUnicode_GET_ENCODING(p));
+		printf("  length: %zd\n", PyUnicode_GET_LENGTH(p));
+		printf("  value: %s\n", PyUnicode_AsUTF8(p));
+	}
+	else
+		printf("  [ERROR] Invalid String Object\n");
+}
