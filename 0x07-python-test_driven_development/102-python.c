b29ca798a250597a30724059f41e1ee21b7df6bb
badreaz <baz.23.2019@gmail.com>
2023-10-02 18:35:54 +0400

    fix compilationn error

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -8,11 +8,11 @@
 void print_python_string(PyObject *p)
 {
 	printf("[.] string object info\n");
-	if (PyUnicode_check(p))
+	if (PyUnicode_Check(p))
 	{
-		printf("  type: %s\n", PyUnicode_GET_ENCODING(p));
+		printf("  type: %s\n", (char *)PyUnicode_GET_ENCODING(p));
 		printf("  length: %zd\n", PyUnicode_GET_LENGTH(p));
-		printf("  value: %s\n", PyUnicode_AsUTF8(p));
+		printf("  value: %s\n", (char *)PyUnicode_AsUTF8(p));
 	}
 	else
 		printf("  [ERROR] Invalid String Object\n");
