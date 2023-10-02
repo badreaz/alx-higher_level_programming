3669db77dcc5135d54d7504a7f315a93a2fa2d9c
badreaz <baz.23.2019@gmail.com>
2023-10-02 15:05:01 +0400

    create 101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" lazy_matrix_mul definition module """
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ multiplies 2 matrices """
+
+    return numpy.matmul(m_a, m_b)
