a0ce27af198f6ff2d3c994c18a48758f73539336
badreaz <baz.23.2019@gmail.com>
2023-10-17 19:06:53 +0400

    edit syntax

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table 'first_tablle' in the database in MySQL
-CREATE TABLE IF NOT EXISTS first_table { id INT, name VARCHAR(256)};
+CREATE TABLE IF NOT EXISTS 'first_table' { 'id' INT, 'name' VARCHAR(256)};
