83c071f5bb6d1d56b98819e92a6d9508aadf0c66
badreaz <baz.23.2019@gmail.com>
2023-10-18 22:00:15 +0400

    fix name error

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,4 @@
 SELECT tv_genres.name FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter' GROUP BY tv_geres.name ORDER BY tv_genres.name ASC;
+WHERE tv_shows.title = 'Dexter' GROUP BY tv_genres.name ORDER BY tv_genres.name ASC;
