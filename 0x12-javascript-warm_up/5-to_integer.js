b4e6c0cb530abfb34ef5afad9797463e4b12106a
badreaz <baz.23.2019@gmail.com>
2023-11-13 19:19:27 +0400

    delete extra space

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@
 if (process.argv[2] && !isNaN(process.argv[2])) {
   console.log('My number:', parseInt(process.argv[2]));
 } else {
-  console.log('Not  a number');
+  console.log('Not a number');
 }
