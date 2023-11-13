4d070c01e116aa71a805ef234446a80001efc53a
badreaz <baz.23.2019@gmail.com>
2023-11-13 20:10:24 +0400

    create 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+if (!process.argv[2] || process.argv[2] < 2) {
+  console.log(0);
+} else {
+  const num = process.argv.slice(2).map(Number);
+  console.log(num.sort((a, b) => b - a)[1]);
+}
