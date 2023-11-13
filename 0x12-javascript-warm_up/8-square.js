5d25d89ae4847448f377d1729485fce10da6e6a2
badreaz <baz.23.2019@gmail.com>
2023-11-13 19:44:10 +0400

    create 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+if (!process.argv[2] || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const size = process.argv[2];
+  let i = 0;
+  while (i < size) {
+    console.log('X'.repeat(size));
+    i++;
+  }
+}
