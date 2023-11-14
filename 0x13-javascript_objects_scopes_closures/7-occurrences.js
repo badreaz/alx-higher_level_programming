2d952de2d942efae7c2901dab6799e41a206a238
badreaz <baz.23.2019@gmail.com>
2023-11-14 19:22:17 +0400

    edit function name

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let nb = 0;
   list.map((item) => {
     if (item === searchElement) {
