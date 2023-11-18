8b6e93941713d7f724077c486389cc6233a0b421
badreaz <baz.23.2019@gmail.com>
2023-11-18 18:46:03 +0400

    create 9-model_state_filer_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that lists all State objects that contain
+the letter 'a' from the database hbtn_0e_6_usa """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    """ list objects that start with 'a' """
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for ins in session.query(State).filterr(State.name.contains('a')):
+        print("{}: {}".format(ins.id, ins.name))
