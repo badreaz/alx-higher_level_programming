0ebf01b39b0e0d54f54385f94df08f33c84427fb
badreaz <baz.23.2019@gmail.com>
2023-11-18 12:33:07 +0400

    fix code style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,9 @@ from sys import argv
 if __name__ == '__main__':
     """ list 'states' """
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1],
+            passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
