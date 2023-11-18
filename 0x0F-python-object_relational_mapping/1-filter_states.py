a6c26028c576e91ab63d2b3676d43b6a9a688aa6
badreaz <baz.23.2019@gmail.com>
2023-11-18 12:44:47 +0400

    create 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that list all 'states' with
+a 'name' starting with 'N' from the database
+hbtn_0e_0_usa """
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """ list names that start with 'N' """
+
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1],
+            passwd=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHILE name '^N'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
