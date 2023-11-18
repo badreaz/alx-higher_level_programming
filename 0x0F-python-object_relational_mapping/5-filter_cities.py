356d534b442eb140001cce0a9766aee909a1f72c
badreaz <baz.23.2019@gmail.com>
2023-11-18 14:37:30 +0400

    edit syntax

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("""SELECT cities.namei, cities.id FROM cities JOIN states\
+    cur.execute("""SELECT cities.name, cities.id FROM cities JOIN states\
             ON cities.state_id = states.id\
             WHERE states.name LIKE BINARY $(state)s\
             ORDER BY cities.id ASC""", {'name': argv[4]})
