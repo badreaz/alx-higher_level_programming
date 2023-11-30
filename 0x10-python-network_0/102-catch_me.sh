25008c481cb97521a4d9d1efa15aed2bc2a030a7
badreaz <baz.23.2019@gmail.com>
2023-11-30 23:09:37 +0400

    delete extra line

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with
-# message containing 'You got me!', in the body of the response
+# makes a request to 0.0.0.0:5000/catch_me that contains  'You got me!'
 curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
