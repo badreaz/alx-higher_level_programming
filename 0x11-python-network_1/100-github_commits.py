394f2cdb32e01c9f3aef2176e58a3ce499e94301
badreaz <baz.23.2019@gmail.com>
2023-12-01 20:34:55 +0400

    fix code style

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,11 +7,11 @@ from sys import argv
 
 if __name__ == '__main__':
     r = requests.get("https://api.github.com/repos/{}/{}/commits"
-            .format(argv[2], argv[1]))
+                     .format(argv[2], argv[1]))
     dic = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(dic[i].get("sha"),
-                dic[i].get("commit").get("author").get("name")))
+            print("{}: {}".format(dic[i].get("sha"), dic[i]
+                                  .get("commit").get("author").get("name")))
     except IndexError:
         pass
