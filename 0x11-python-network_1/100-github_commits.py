392e9cc4a2666b3d9b509b80f1c0ee4e2d3b876d
badreaz <baz.23.2019@gmail.com>
2023-12-01 20:30:37 +0400

    create 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" solve the challenge """
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = requests.get("https://api.github.com/repos/{}/{}/commits"
+            .format(argv[2], argv[1]))
+    dic = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(dic[i].get("sha"),
+                dic[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
