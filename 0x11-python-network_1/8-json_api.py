bdc90a99d149ea216e688b85ba14fe07aa252c47
badreaz <baz.23.2019@gmail.com>
2023-12-01 19:57:38 +0400

    create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a paramete """
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    if (len(argv) == 1):
+        q = ""
+    else:
+        q = argv[2]
+    data = {"q": q}
+    r = requests.post("http://0.0.0.0:5000/search_user", data)
+    try:
+        result = r.json()
+        if result == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(result.get("id"), result.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
