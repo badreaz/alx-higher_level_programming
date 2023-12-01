36a0f4ed11c2469dac01ce0c20a5d850ac8f9b7b
badreaz <baz.23.2019@gmail.com>
2023-12-01 20:12:01 +0400

    create 10-my_githup.py

---
+++ 0x11-python-network_1/10-my_githup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+
+import requests
+from sys import argv
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == '__main__':
+    basic_auth = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get("https://api.githup.com/user", basic_auth)
+    id = r.json().get("id")
+    print(id)
