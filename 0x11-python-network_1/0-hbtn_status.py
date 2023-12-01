8c166fdaac88fad8bc1084356a9320996e51222d
badreaz <baz.23.2019@gmail.com>
2023-12-01 18:35:26 +0400

    create 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status """
+
+from urllib import request
+
+
+if __name__ == '__main__':
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
